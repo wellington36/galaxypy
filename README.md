33c9d836b561c0a5a613e5504ed5203edc8dad99
Wellington <wellington.71319@gmail.com>
2022-01-19 18:20:58 -0300

    Update code

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Personal version of galaxy project for learn and fun.
 
 # Usage
 
-Run (with python3.9 because the `kivy` library might not work well.)
+Run (with python3.9 because the `kivy` library might not work well with python3.10).
 
 ```bash
 python3.9 main.py
--- main.py
+++ main.py
@@ -9,32 +9,37 @@ class MainWidget(Widget):
 	perspective_point_x = NumericProperty(0)
 	perspective_point_y = NumericProperty(0)
 	
-	V_NB_LINES = 7
-	V_LINES_SPACING = 0.1	# persentage in screen width
+	V_NB_LINES = 7		# number of lines
+	V_LINES_SPACING = 0.3		# persentage in screen width
 	vertical_lines = []
 	
 	def __init__(self, **kwargs):
 		super(MainWidget, self).__init__(**kwargs)
-		#print("INIT W:" + str(self.width) + " H:" + str(self.height))
+		# print("INIT W:" + str(self.width) + " H:" + str(self.height))
 		self.init_vertical_lines()
+	
 	
 	def on_parent(self, widget, parent):
 		#print("ON PARENT W:" + str(self.width) + " H:" + str(self.height))
 		pass
 	
-	def on_size(self, *args):
+	
+	def on_size(self, *args):		# update perspective points and lines
 		#print("ON SIZE W:" + str(self.width) + " H:" + str(self.height))
 		self.perspective_point_x = self.width/2
 		self.perspective_point_y = self.height * 0.75
 		self.update_vertical_lines()
 	
+	
 	def on_perspective_point_x(self, widget, value):
 		# print("PX: " + str(value))
 		pass
 
+
 	def on_perspective_point_y(self, widget, value):
 		#print("PY: " + str(value))
 		pass
+
 	
 	def init_vertical_lines(self):
 		with self.canvas:
@@ -42,6 +47,7 @@ class MainWidget(Widget):
 			# self.line = Line(points=[self.width/2, 0, self.width/2, 100])
 			for i in range(0, self.V_NB_LINES):
 				self.vertical_lines.append(Line())
+
 		
 	def update_vertical_lines(self):
 		central_line_x = int(self.width / 2)
@@ -50,12 +56,37 @@ class MainWidget(Widget):
 		
 		for i in range(0, self.V_NB_LINES):
 			line_x = central_line_x + offset * spacing
-			self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+			
+			x1, y1 = self.transform(line_x, 0)
+			x2, y2 = self.transform(line_x, self.height)
+			
+			self.vertical_lines[i].points = [x1, y1, x2, y2]
 			offset += 1
+	
+	
+	def transform(self, x, y):
+		# return self.transform_2D(x, y)
+		return self.tranform_perspective(x, y)
+	
+	
+	def transform_2D(self, x, y):
+		return int(x), int(y)
 
+
+	def tranform_perspective(self, x, y):
+		tr_y = y * self.perspective_point_x / self.height
+		
+		if tr_y > self.perspective_point_y:
+			tr_y = self.perspective_point_y
+		
+		return int(x), int(tr_y)
+	
+	
 
 class GalaxyApp(App):
 	pass
 	
 
 GalaxyApp().run()
+
+
