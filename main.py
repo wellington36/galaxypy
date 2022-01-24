aeb614994f28a4a426137350f4849dba398f5885
wellington36 <wellington.71319@gmail.com>
2022-01-24 19:14:23 -0300

    Add comands for moviment

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from kivy.config import Config
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
+
+from kivy import platform
 from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.properties import Clock
 from kivy.properties import NumericProperty
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
 from kivy.graphics.vertex_instructions import Line
 from kivy.graphics.context_instructions import Color
 
@@ -10,6 +16,8 @@ class MainWidget(Widget):
 	perspective_point_x = NumericProperty(0)
 	perspective_point_y = NumericProperty(0)
 	current_offset_y = NumericProperty(0)
+	current_offset_x = NumericProperty(0)
+	current_speed_x = NumericProperty(0)
 	
 	V_NB_LINES = 8		# number of lines
 	V_LINES_SPACING = 0.25		# persentage in screen width
@@ -20,13 +28,32 @@ class MainWidget(Widget):
 	horizontal_lines = []
 
 	SPEED = 4
+	SPEED_X = 4
 	
 	def __init__(self, **kwargs):
 		super(MainWidget, self).__init__(**kwargs)
-		# print("INIT W:" + str(self.width) + " H:" + str(self.height))
 		self.init_vertical_lines()
 		self.init_horizontal_lines()
-		Clock.schedule_interval(self.update, 1.0 / 60)
+
+		if self.is_desktop():
+			self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
+			self._keyboard.bind(on_key_down=self.on_keyboard_down)
+			self._keyboard.bind(on_key_up=self.on_keyboard_up)
+
+		Clock.schedule_interval(self.update, 1.0 / 60)	# 60 fps
+
+
+	def keyboard_closed(self):
+		self._keyboard_unbind(on_key_down=self.on_keyboard_down)
+		self._keyboard_unbind(on_key_up=self.on_keyboard_up)
+		self._keyboard = None
+	
+
+	def is_desktop(self):
+		if platform in ('linux', 'win', 'macosc'):
+			return True
+		else:
+			return False
 	
 	
 	def on_parent(self, widget, parent):
@@ -75,7 +102,7 @@ class MainWidget(Widget):
 		offset = -int(self.V_NB_LINES/2) + 0.5	# -n ... 0 ... +n
 		
 		for i in range(0, self.V_NB_LINES):
-			line_x = central_line_x + offset * spacing
+			line_x = central_line_x + offset * spacing + self.current_offset_x
 			
 			x1, y1 = self.transform(line_x, 0)
 			x2, y2 = self.transform(line_x, self.height)
@@ -89,8 +116,8 @@ class MainWidget(Widget):
 		spacing = self.V_LINES_SPACING * self.width
 		offset = -int(self.V_NB_LINES / 2) + 0.5
 
-		xmin = central_line_x + offset*spacing
-		xmax = central_line_x - offset*spacing
+		xmin = central_line_x + offset*spacing + self.current_offset_x
+		xmax = central_line_x - offset*spacing + self.current_offset_x
 		spacing_y = self.H_LINES_SPACING * self.height
 		
 		for i in range(0, self.V_NB_LINES):
@@ -126,17 +153,44 @@ class MainWidget(Widget):
 		tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
 
 		return int(tr_x), int(tr_y)
+
+
+	def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+		if keycode[1] == 'left':
+			self.current_speed_x = self.SPEED_X
+		elif keycode[1] == 'right':
+			self.current_speed_x = -self.SPEED_X
+		
+		return True
+	
+
+	def on_keyboard_up(self, keyboard, keycode):
+		self.current_speed_x = 0
+		return True
 	
 	
+	def on_touch_down(self, touch):
+		if touch.x < self.width / 2:
+			self.current_speed_x = self.SPEED_X
+		else:
+			self.current_speed_x = -self.SPEED_X
+
+
+	def on_touch_up(self, touch):
+		self.current_speed_x = 0
+
+
 	def update(self, dt):
-		#print("update")
+		time_factor = dt*60
 		self.update_vertical_lines()
 		self.update_horizontal_lines()
-		self.current_offset_y += self.SPEED
+		self.current_offset_y += self.SPEED * time_factor
 
 		spacing_y = self.H_LINES_SPACING * self.height
 		if self.current_offset_y >= spacing_y:
 			self.current_offset_y -= spacing_y
+
+		self.current_offset_x += self.current_speed_x * self.SPEED_X * time_factor
 
 
 class GalaxyApp(App):
