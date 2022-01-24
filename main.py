e4f9dbf732137958de0233471888ec5432f32d5c
wellington36 <wellington.71319@gmail.com>
2022-01-24 19:36:53 -0300

    Clear code

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ from kivy.graphics.context_instructions import Color
 
 
 class MainWidget(Widget):
+	from transforms import transform, tranform_perspective, transform_2D
+	from user_actions import keyboard_closed, on_keyboard_down, on_keyboard_up, on_touch_down, on_touch_up
+
 	perspective_point_x = NumericProperty(0)
 	perspective_point_y = NumericProperty(0)
 	current_offset_y = NumericProperty(0)
@@ -41,12 +44,6 @@ class MainWidget(Widget):
 			self._keyboard.bind(on_key_up=self.on_keyboard_up)
 
 		Clock.schedule_interval(self.update, 1.0 / 60)	# 60 fps
-
-
-	def keyboard_closed(self):
-		self._keyboard_unbind(on_key_down=self.on_keyboard_down)
-		self._keyboard_unbind(on_key_up=self.on_keyboard_up)
-		self._keyboard = None
 	
 
 	def is_desktop(self):
@@ -55,31 +52,7 @@ class MainWidget(Widget):
 		else:
 			return False
 	
-	
-	def on_parent(self, widget, parent):
-		#print("ON PARENT W:" + str(self.width) + " H:" + str(self.height))
-		pass
-	
-	
-	def on_size(self, *args):		# update perspective points and lines
-		#print("ON SIZE W:" + str(self.width) + " H:" + str(self.height))
-		self.perspective_point_x = self.width/2
-		self.perspective_point_y = self.height * 0.75
-		#self.update_vertical_lines()
-		#self.update_horizontal_lines()
-		pass
-	
-	
-	def on_perspective_point_x(self, widget, value):
-		# print("PX: " + str(value))
-		pass
 
-
-	def on_perspective_point_y(self, widget, value):
-		#print("PY: " + str(value))
-		pass
-
-	
 	def init_vertical_lines(self):
 		with self.canvas:
 			Color(1, 1, 1)
@@ -128,60 +101,13 @@ class MainWidget(Widget):
 			
 			self.horizontal_lines[i].points = [x1, y1, x2, y2]
 
-	
-	def transform(self, x, y):
-		#return self.transform_2D(x, y)
-		return self.tranform_perspective(x, y)
-	
-	
-	def transform_2D(self, x, y):
-		return int(x), int(y)
-
-
-	def tranform_perspective(self, x, y):
-		lin_y = y * self.perspective_point_y / self.height
-		
-		if lin_y > self.perspective_point_y:
-			lin_y = self.perspective_point_y
-
-		diff_x = x - self.perspective_point_x
-		diff_y = self.perspective_point_y - lin_y
-		factor_y = diff_y/self.perspective_point_y
-		factor_y = pow(factor_y, 3)
-
-		tr_x = self.perspective_point_x + diff_x*factor_y
-		tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
-
-		return int(tr_x), int(tr_y)
-
-
-	def on_keyboard_down(self, keyboard, keycode, text, modifiers):
-		if keycode[1] == 'left':
-			self.current_speed_x = self.SPEED_X
-		elif keycode[1] == 'right':
-			self.current_speed_x = -self.SPEED_X
-		
-		return True
-	
-
-	def on_keyboard_up(self, keyboard, keycode):
-		self.current_speed_x = 0
-		return True
-	
-	
-	def on_touch_down(self, touch):
-		if touch.x < self.width / 2:
-			self.current_speed_x = self.SPEED_X
-		else:
-			self.current_speed_x = -self.SPEED_X
-
-
-	def on_touch_up(self, touch):
-		self.current_speed_x = 0
-
 
 	def update(self, dt):
 		time_factor = dt*60
+
+		self.perspective_point_x = self.width/2
+		self.perspective_point_y = self.height * 0.75
+
 		self.update_vertical_lines()
 		self.update_horizontal_lines()
 		self.current_offset_y += self.SPEED * time_factor
@@ -198,5 +124,4 @@ class GalaxyApp(App):
 	
 
 GalaxyApp().run()
-
 
